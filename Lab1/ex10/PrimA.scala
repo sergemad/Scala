9f5bb41e873f7302c950b47541426857bdbf2ad9
sergemad <sergekeita01@gmail.com>
2022-04-13 14:42:09 +0200

    exercice 16 & 17

--- Lab1/ex10/PrimA.scala
+++ Lab1/ex10/PrimA.scala
@@ -10,15 +10,16 @@ surname:String, date_of_birth:String): Unit ={
     println(map)
 }
 
-def Exercice12(filename:String): Unit ={
+def Exercice12(filename:String): Map[String,String] ={
     var result_line:Array[String] = Array()
     var res:List[String] = List()
     for (line <- fromFile(filename).getLines()){
         result_line = line.split(':')
         res = result_line(1) :: res
     }
-    var map : Map[String, String] = Map("name" -> "", "surname" -> "", "date of birth" -> "")
-    Exercice11(map, res(2),res(1),res(0))
+    var map : Map[String, String] = Map("name" -> res(2), "surname" -> res(1), "date of birth" -> res(0))
+    //Exercice11(map, res(2),res(1),res(0))
+    return map
 }
 
 
@@ -44,20 +45,55 @@ def Exercice15(filename:String): Map[String,String] ={
 }
 
 
-def Exercice16(map:Map[String,String], date:String): Unit ={
+def Exercice16(map:Map[String,String], date:String): Map[String,String] ={
     var temp:Map[String,String] = Map()
 
-    map.foreach((e: (String, String)) => 
-    println(String.toString(e._1.split(" ")))
-    )
-    //if (date == e._1.split(" ")[0]){
-    //    temp = (e._1 -> e._2)
-    //})
-    println(temp)
+    for ((e: (String, String)) <- map){
+        var date_to_compare:Array[String] = e._1.split(" ")
+        if (date == date_to_compare(0)){
+            temp += (e._1 -> e._2)
+        }
+    }
+    return temp
+}
+
+def Exercice17(userinfo: Map[String,String],useract: Map[String,String],date: String): List[String] = {
+    var res:List[String] = List()
+    var username:String = ""
+    var date_of_birth:String = ""
+    for ((e: (String, String)) <- userinfo){
+        if (e._1 == "name"){
+            username += e._2
+        }
+        else if (e._1 == "surname"){
+            username += " " + e._2
+        }
+        else {
+            date_of_birth = e._2
+        }
+    }
+    if (date_of_birth == date){
+        res = s"Happy birthday $username !" :: res
+    }
+    else{
+        res = s"Welcome $username !" :: res
+    }
+    
+    var act_scheduled:Map[String,String] = Exercice16(useract,date)
+    for ((e: (String, String)) <- act_scheduled){
+        var v1:String = e._1
+        var v2:String = e._2
+        res = s"You have $v2 at $v1" :: res
+    }
+
+    return res
 }
 
 var map : Map[String, String] = Exercice15("/Users/sergekeita/Desktop/Learning/M1_Efrei/Scala/Lab1/ex10/userActivities.txt")
-Exercice16(map,"20220404")
+var map1 : Map[String,String] = Exercice12("/Users/sergekeita/Desktop/Learning/M1_Efrei/Scala/Lab1/ex10/userInfo.txt")
+var res : List[String] = Exercice17(map1,map,"20220404")
+println(res)
+//Exercice16(map,"20220404")
 //Exercice15("/Users/sergekeita/Desktop/Learning/M1_Efrei/Scala/Lab1/ex10/userActivities.txt")
 //Exercice14(map)
 //Exercice12("/Users/sergekeita/Desktop/Learning/M1_Efrei/Scala/Lab1/ex10/userInfo.txt")
